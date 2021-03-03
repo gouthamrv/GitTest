75f0c168d46c570e1f35321e6b20d4123bd39ea7
mymac <mymac@192.168.1.9>
2021-03-03 15:04:30 -0800

    Initial commit of project.

---
+++ GitTest/src/com/test/Test.java
@@ -0,0 +1,17 @@
+package com.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SalesData sd = new SalesData();
+		sd.someData();
+		displayGreeting();
+	}
+	
+	public static void displayGreeting() {
+		System.out.println("testing main");
+	}
+	
+
+}
